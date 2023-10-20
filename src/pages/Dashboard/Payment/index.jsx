e083e4bf534900070bcac1121e7a562646da75ed
breno.breves <brenocsb@dcc.ufrj.br>
2023-10-20 19:52:12 -0300

    Criação de Ticket completa

--- src/pages/Dashboard/Payment/index.jsx
+++ src/pages/Dashboard/Payment/index.jsx
@@ -1,16 +1,34 @@
 import { useContext, useEffect, useState } from "react";
 import UserContext from "../../../contexts/UserContext";
 import styled from "styled-components";
-import { getPersonalInformations } from "../../../services/enrollmentApi";
 import useEnrollment from "../../../hooks/api/useEnrollment";
+import { createTicket } from "../../../services/ticketApi";
+import useTicket from "../../../hooks/api/useTicket";
 
 export default function Payment() {
   const {userData, setUserData} = useContext(UserContext)
   const {enrollment} = useEnrollment() 
+  const {ticket} = useTicket()
   const [ticketType, setTicketType] = useState(0)
+  async function reserveTicket(){
+    const request = createTicket(ticketType,userData.token)
+      .then((res)=>{
+        console.log(res)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+  }
+  useEffect(()=>{
+    if (ticket) {
+      setTicketType(ticket.ticketTypeId)
+    }
+  },[ticket,ticketType,setTicketType])
   return(
     <>
       <SCPageHeader>Ingresso e pagamento</SCPageHeader>
+      {ticket ? "":
+      <>
       {!enrollment ? 
       <SCNotEnrolled>
         <SCNotEnrolledMsg>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</SCNotEnrolledMsg>
@@ -21,8 +39,8 @@ export default function Payment() {
         Primeiro, escolha sua modalidade de ingresso
       </SCTicketTypeHeader>
       <SCButtonContainer>
-        <SCOptionButton selected={ticketType > 1} onClick={()=>setTicketType(4)}>Presencial<SCPriceSpan>R$ 550</SCPriceSpan></SCOptionButton>
-        <SCOptionButton selected={ticketType === 1} onClick={()=>setTicketType(1)}>Online<SCPriceSpan>R$ 500</SCPriceSpan></SCOptionButton>
+        <SCOptionButton disabled={ticket} selected={ticketType > 1} onClick={()=>setTicketType(4)}>Presencial<SCPriceSpan>R$ 550</SCPriceSpan></SCOptionButton>
+        <SCOptionButton disabled={ticket} selected={ticketType === 1} onClick={()=>setTicketType(1)}>Online<SCPriceSpan>R$ 500</SCPriceSpan></SCOptionButton>
       </SCButtonContainer>
       {ticketType > 1 ?
         <>
@@ -30,12 +48,21 @@ export default function Payment() {
             Ótimo! Agora escolha sua modalidade de hospedagem
           </SCTicketTypeHeader>
           <SCButtonContainer>
-            <SCOptionButton selected={ticketType === 2} onClick={()=>setTicketType(2)}>Sem Hotel<SCPriceSpan>+ R$ 0</SCPriceSpan></SCOptionButton>
-            <SCOptionButton selected={ticketType === 3} onClick={()=>setTicketType(3)}>Com Hotel<SCPriceSpan>+ R$ 100</SCPriceSpan></SCOptionButton>
+            <SCOptionButton disabled={ticket} selected={ticketType === 2} onClick={()=>setTicketType(2)}>Sem Hotel<SCPriceSpan>+ R$ 0</SCPriceSpan></SCOptionButton>
+            <SCOptionButton disabled={ticket} selected={ticketType === 3} onClick={()=>setTicketType(3)}>Com Hotel<SCPriceSpan>+ R$ 100</SCPriceSpan></SCOptionButton>
           </SCButtonContainer>
         </> 
        : ""}
-       
+       {ticketType > 0 && ticketType < 4 ? 
+        <>
+          <SCTicketTypeHeader>
+            Fechado! O total ficou em R$ {500 + (ticketType > 1)*50 + (ticketType === 3)*100}. Agora é só confirmar:
+          </SCTicketTypeHeader>
+          <SCReserveButton onClick={reserveTicket}>RESERVAR INGRESSO</SCReserveButton>
+        </>
+       :""}
+      </>
+      }
       </>
       }
     </>
@@ -101,7 +128,7 @@ export default function Payment() {
     gap:24px;
     justify-content:flex-start;
     align-items:flex-start;
-    margin-bottom:44px;
+    margin-bottom:35px;
   `
   const SCPriceSpan = styled.span`
     color: #898989;
@@ -111,4 +138,12 @@ export default function Payment() {
     font-style: normal;
     font-weight: 400;
     line-height: normal; 
+  `
+  const SCReserveButton = styled.button`
+    width: 162px;
+    height: 37px; 
+    border-radius: 4px;
+    background: #E0E0E0;
+    border: none;
+    box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.25);
   `
