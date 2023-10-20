b68919292ba490b546a8bb4aec58f015d1961340
breno.breves <brenocsb@dcc.ufrj.br>
2023-10-20 18:14:33 -0300

    Quase tudo feito de Ingresso, falta só o final de confirmação.

--- src/pages/Dashboard/Payment/index.jsx
+++ src/pages/Dashboard/Payment/index.jsx
@@ -7,6 +7,7 @@ import useEnrollment from "../../../hooks/api/useEnrollment";
 export default function Payment() {
   const {userData, setUserData} = useContext(UserContext)
   const {enrollment} = useEnrollment() 
+  const [ticketType, setTicketType] = useState(0)
   return(
     <>
       <SCPageHeader>Ingresso e pagamento</SCPageHeader>
@@ -14,7 +15,28 @@ export default function Payment() {
       <SCNotEnrolled>
         <SCNotEnrolledMsg>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</SCNotEnrolledMsg>
       </SCNotEnrolled> 
-      : "Escolher o tipo de ingresso em breve!"}
+      :
+      <>
+      <SCTicketTypeHeader>
+        Primeiro, escolha sua modalidade de ingresso
+      </SCTicketTypeHeader>
+      <SCButtonContainer>
+        <SCOptionButton selected={ticketType > 1} onClick={()=>setTicketType(4)}>Presencial<SCPriceSpan>R$ 550</SCPriceSpan></SCOptionButton>
+        <SCOptionButton selected={ticketType === 1} onClick={()=>setTicketType(1)}>Online<SCPriceSpan>R$ 500</SCPriceSpan></SCOptionButton>
+      </SCButtonContainer>
+      {ticketType > 1 ?
+        <>
+          <SCTicketTypeHeader>
+            Ótimo! Agora escolha sua modalidade de hospedagem
+          </SCTicketTypeHeader>
+          <SCButtonContainer>
+            <SCOptionButton selected={ticketType === 2} onClick={()=>setTicketType(2)}>Sem Hotel<SCPriceSpan>+ R$ 0</SCPriceSpan></SCOptionButton>
+            <SCOptionButton selected={ticketType === 3} onClick={()=>setTicketType(3)}>Com Hotel<SCPriceSpan>+ R$ 100</SCPriceSpan></SCOptionButton>
+          </SCButtonContainer>
+        </> 
+       : ""}
+      </>
+      }
     </>
   )}
 
@@ -25,6 +47,7 @@ export default function Payment() {
     font-size: 34px;
     line-height: 40px;
     color: #000000;
+    margin-bottom:37px;
   `
   const SCNotEnrolled = styled.div`
     width:100%;
@@ -35,7 +58,7 @@ export default function Payment() {
   `
 
   const SCNotEnrolledMsg = styled.div`
-    font-family: Roboto;
+    font-family: "Roboto",sans-serif  ;
     font-size: 20px;
     font-weight: 400;
     line-height: 23px;
@@ -44,4 +67,47 @@ export default function Payment() {
     width: 388px;
     color:#8E8E8E;
     padding-bottom:50px;
+  `
+  const SCTicketTypeHeader = styled.div`
+    color: #8E8E8E;
+    font-family: "Roboto",sans-serif;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal; 
+    margin-bottom:17px;
+  `
+  const SCOptionButton = styled.button`
+    border-radius: 20px;
+    border: ${(props) =>(props.selected ? "none":"1px solid #CECECE")}; 
+    width: 145px;
+    height: 145px; 
+    background-color: ${(props) =>(props.selected ? "#FFEED2":"white")};
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    color: #454545;
+    text-align: center;
+    font-family: "Roboto",sans-serif;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal; 
+    flex-direction:column;
+  `
+  const SCButtonContainer = styled.div`
+    display:flex;
+    gap:24px;
+    justify-content:flex-start;
+    align-items:flex-start;
+    margin-bottom:44px;
+  `
+  const SCPriceSpan = styled.span`
+    color: #898989;
+    text-align: center;
+    font-family: "Roboto",sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal; 
   `
